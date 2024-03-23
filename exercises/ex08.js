8b627f00e89e807ca7b134fc4cd312453d0fd417
David Lucas <Daiko1@skiff.com>
2024-03-23 16:52:18 -0300

    exercises
    i did some exercises

---
+++ exercises/ex08.js
@@ -0,0 +1,8 @@
+var vel = 10
+console.log(`a velocidade do seu carro é ${vel}km/h`)
+if (vel > 60) {
+    console.log(`você passou do limite de velocidade. MULTADO.`)
+}
+else {
+    console.log(`limite de velocidade não atingido`)
+}
